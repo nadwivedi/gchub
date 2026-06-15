682296cfe6b3e6967f1ade4c8062da881d866b7e
Narayandwivedi <desinplus1@gmail.com>
2026-06-15 07:43:45 +0530

    make ui fast

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://gchub.in'],
   credentials: true,
 }))
 app.use(cookieParser());
